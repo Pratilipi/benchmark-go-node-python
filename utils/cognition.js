661770e16f261f565347c9dbe3738424dc92a996
Rajat Agarwal <rajat12a@gmail.com>
2018-03-06 15:37:25 +0530

    [CHANGE] data changes

--- utils/cognition.js
+++ utils/cognition.js
@@ -6,20 +6,35 @@ async function getPratilipi_v1( requestObject ) {
   var data = {};
   if( requestObject.slug == null ) {
     //pratilipiId
-    data.libraryData = service_api.getLibraryByReferenceIdAndUserId_v1( requestObject );
-    data.socialData = service_api.getSocialReviewCountByPratilipiIdBatch_v2( requestObject );
-    data.authData = service_api.getAuthByPratilipiIdBatch_v1( requestObject );
-    data.userPratilipiData = service_api.getUserPratilipiByIdBatch_v1( requestObject );
-    data.pratilipiData = await service_api.getPratilipiByIdBatch_v1( requestObject );
-    if( data.pratilipiData.indexOf( null ) !== -1 ) {
-      createErrorObject( 404, 'Pratilipi Not Found' );
+    if( requestObject.userId != 0 ) {
+      data.libraryData = service_api.getLibraryByReferenceIdAndUserId_v1( requestObject );
+      data.socialData = service_api.getSocialReviewCountByPratilipiIdBatch_v2( requestObject );
+      data.authData = service_api.getAuthByPratilipiIdBatch_v1( requestObject );
+      data.userPratilipiData = service_api.getUserPratilipiByIdBatch_v1( requestObject );
+      data.pratilipiData = await service_api.getPratilipiByIdBatch_v1( requestObject );
+      if( data.pratilipiData.indexOf( null ) !== -1 ) {
+        createErrorObject( 404, 'Pratilipi Not Found' );
+      }
+      requestObject.authorId = data.pratilipiData.map( function( pratilipiObject ) { return pratilipiObject.authorId; } ).toString();
+      data.authorData = await service_api.getAuthorByIdBatch_v1( requestObject );
+      data.socialData = await data.socialData;
+      data.authData = await data.authData;
+      data.libraryData = await data.libraryData;
+      data.userPratilipiData = await data.userPratilipiData;
+    } else {
+      data.socialData = service_api.getSocialReviewCountByPratilipiIdBatch_v2( requestObject );
+      data.pratilipiData = await service_api.getPratilipiByIdBatch_v1( requestObject );
+      if( data.pratilipiData.indexOf( null ) !== -1 ) {
+        createErrorObject( 404, 'Pratilipi Not Found' );
+      }
+      requestObject.authorId = data.pratilipiData.map( function( pratilipiObject ) { return pratilipiObject.authorId; } ).toString();
+      data.authorData = await service_api.getAuthorByIdBatch_v1( requestObject );
+      data.socialData = await data.socialData;
+      data.authData = [];
+      data.libraryData = [];
+      data.userPratilipiData = [];
     }
-    requestObject.authorId = data.pratilipiData.map( function( pratilipiObject ) { return pratilipiObject.authorId; } ).toString();
-    data.authorData = await service_api.getAuthorByIdBatch_v1( requestObject );
-    data.socialData = await data.socialData;
-    data.authData = await data.authData;
-    data.libraryData = await data.libraryData;
-    data.userPratilipiData = await data.userPratilipiData;
+    
   } else {
     //slug
     data.pratilipiData = await service_api.getPratilipiBySlugBatch_v1( requestObject );
