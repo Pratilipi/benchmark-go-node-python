3bb9dc8556325f36e2681abb1c305a909a92fb25
sachinpatil <sachinnitw1317@gmail.com>
2019-01-11 03:23:07 +0530

    minor error handling added

--- worker.py
+++ worker.py
@@ -49,29 +49,32 @@ def pushEventToDB(msg):
 if __name__ == '__main__':
 
     while True:
-
         try:
-            print("poll start")
-            msg = c.poll(1)
-            print("polled")
+            try:
+                print("poll start")
+                msg = c.poll(1)
+                print("polled")
 
-        except SerializerError as e:
-            print("Message deserialization failed for {}: {}".format(msg, e))
-            break
-
-        if msg is None:
-            print("no new msg")
-            continue
-
-        if msg.error():
-            if msg.error().code() == KafkaError._PARTITION_EOF:
-                continue
-            else:
-                print(msg.error())
+            except SerializerError as e:
+                print("Message deserialization failed for {}: {}".format(msg, e))
                 break
 
-        print("{} {} {}".format(msg.key(), msg.value(), msg.partition()))
-        pushEventToDB(msg.value())
+            if msg is None:
+                print("no new msg")
+                continue
+
+            if msg.error():
+                if msg.error().code() == KafkaError._PARTITION_EOF:
+                    continue
+                else:
+                    print(msg.error())
+                    break
+
+            print("{} {} {}".format(msg.key(), msg.value(), msg.partition()))
+            pushEventToDB(msg.value())
+        except Exception as err:
+            print(err)
+            continue
         # futures.append(pool.submit(pushEventToDB, msg.value()))
         #
         # if len(futures)%1000 == 0:
